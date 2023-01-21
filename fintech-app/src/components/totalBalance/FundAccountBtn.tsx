430726202f355fd82c9d36aac3431a0e90aef952
Chukwudi Nweze <45886893+chukwudinweze@users.noreply.github.com>
2023-01-21 16:30:33 +0100

    Implemented changes to improve the Fund Account button in the Total Balance component. Updated  TotalBalance.tsx, index.tsx, and yarn.lock to reflect the changes. Added new store folder for improved state management.

--- fintech-app/src/components/totalBalance/FundAccountBtn.tsx
+++ fintech-app/src/components/totalBalance/FundAccountBtn.tsx
@@ -1,6 +1,15 @@
 import AddIcon from "@mui/icons-material/Add";
 import styles from "./FundAccountBtn.module.css";
+
+import { withdraw } from "../../store";
+import { useDispatch } from "react-redux/es/exports";
+
 const FundAccountBtn = () => {
+  const dispatch = useDispatch();
+
+  const fundAccountHandler = () => {
+    dispatch(withdraw(100));
+  };
   return (
     <button className={styles.FundAccountBtn}>
       <AddIcon className={styles.AddIcon} />

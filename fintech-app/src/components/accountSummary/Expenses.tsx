0c54cd6f125c213eddf141f33bf1fb0206a3f5e8
Chukwudi Nweze <45886893+chukwudinweze@users.noreply.github.com>
2023-02-11 15:31:44 +0100

    Refactored Expenses component and made related updates to App, TypeOfTransaction, NotificationPage, Bills, ConfirmTransaction, ExperimentTwo, AccountSummary, PieChart, and completedTxnSlice. Added LoanPage, loan and no-credit-card assets, Loan components and transactionDate utility

--- fintech-app/src/components/accountSummary/Expenses.tsx
+++ fintech-app/src/components/accountSummary/Expenses.tsx
@@ -1,9 +1,14 @@
-import { Box } from "@mui/material";
+import { Avatar, Box, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
 import PageHeader from "../../Global/PageHeader";
+import { useAppSelector } from "../../store/hooks";
 import Expense from "./Expense";
 import PieChart from "./PieChart";
+import noTransactionImg from "../../assets/no-credit-card.png";
+import { Link } from "react-router-dom";
 
 export default function App() {
+  const transactions = useAppSelector((state) => state.completedTransactions);
   return (
     <>
       <Box
@@ -11,14 +16,62 @@ export default function App() {
         flexDirection="column"
         alignItems="flex-start"
         minHeight="100vh"
+        bgcolor={transactions.length !== 0 ? "#6236ff" : "#fff"}
         sx={{
-          background: "#6236ff",
           width: "100%",
         }}
       >
-        <PageHeader label="Your Expenses" color="#fff" />
-        <PieChart />
-        <Expense />
+        <PageHeader
+          label="Your Expenses"
+          color={transactions.length !== 0 ? "#fff" : "#6236ff"}
+        />
+        {transactions.length !== 0 ? (
+          <Box sx={{ width: "100%" }}>
+            <PieChart />
+            <Expense />
+          </Box>
+        ) : (
+          <Box
+            display="flex"
+            justifyContent="center"
+            minHeight="100vh"
+            sx={{ background: "#fff" }}
+            paddingTop="150px"
+          >
+            <Stack
+              spacing={2}
+              width="100%"
+              alignItems="center"
+              padding="0 20px"
+            >
+              <Avatar
+                alt="Remy Sharp"
+                src={noTransactionImg}
+                sx={{
+                  width: { xs: "150px", sm: "200px" },
+                  height: { xs: "150px", sm: "200px" },
+                  padding: "5px",
+                  background: "#6236ff",
+                  border: "1px solid #6236ff",
+                }}
+              />
+              <Typography textAlign="center" variant="h6" fontWeight={700}>
+                No Transaction Recorded Yet
+              </Typography>
+              <Box textAlign="center">
+                <Typography variant="body1">
+                  Get started with Quickpay by funding your account. Withdraw,
+                  transfer, exchange currency, and pay bills easily.
+                  <Typography fontWeight="700" color="#6236ff">
+                    <Link color="#6236ff" to="/">
+                      Start now!
+                    </Link>
+                  </Typography>
+                </Typography>
+              </Box>
+            </Stack>
+          </Box>
+        )}
       </Box>
     </>
   );
